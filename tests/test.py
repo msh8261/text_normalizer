a870d45b4a346ad163330751a2331e3d72dca7cd
mohser sh <sh1361m@gmail.com>
2021-02-20 18:18:22 +0800

    fix

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,10 @@
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
 
-from handler import normalize_corpus, remove_stopwords, remove_special_characters
-from handler import remove_accented_chars, expand_contractions, lemmatize_text 
-from handler import remove_repeated_characters, simple_porter_stemming, strip_html_tags
-
+from handler import *
 
 
 examples = 'Hello, my name is kaka, You saw my father. Prepare to see'
