fb62d839247fb6e87d493158bfbaff82156b34b5
mohser sh <sh1361m@gmail.com>
2021-02-23 12:05:24 +0800

    fix

--- handler.py
+++ handler.py
@@ -180,23 +180,23 @@ def lambda_handler(event, context):
 
 
 
-# input_data =  {"body": ["US unveils world's most powerful supercomputer, beats China. ", 
-#                "The US has unveiled the world's most powerful supercomputer called 'Summit', ", 
-#                "beating the previous record-holder China's Sunway TaihuLight. With a peak performance ",
-#                "of 200,000 trillion calculations per second, it is over twice as fast as Sunway TaihuLight, ",
-#                "which is capable of 93,000 trillion calculations per second. Summit has 4,608 servers, ",
-#                "which reportedly take up the size of two tennis courts." ,   
-#             ]
-#         }
+input_data =  {"body": ["US unveils world's most powerful supercomputer, beats China. ", 
+               "The US has unveiled the world's most powerful supercomputer called 'Summit', ", 
+               "beating the previous record-holder China's Sunway TaihuLight. With a peak performance ",
+               "of 200,000 trillion calculations per second, it is over twice as fast as Sunway TaihuLight, ",
+               "which is capable of 93,000 trillion calculations per second. Summit has 4,608 servers, ",
+               "which reportedly take up the size of two tennis courts." ,   
+            ]
+        }
 
 
-# res = lambda_handler(input_data, "")
-# print(res["body"])
+res = lambda_handler(input_data, "")
+print(res["body"])
 
 
-# examples = 'Hello, my name is kaka, You saw my father. Prepare to see'
-# text = lemmatize_text((examples))
-# print(text)
+examples = 'Hello, my name is kaka, You saw my father. Prepare to see'
+text = lemmatize_text((examples))
+print(text)
 
 
 
